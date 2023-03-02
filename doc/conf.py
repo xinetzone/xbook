248cbe762800d8e9cf06ae34ec5b2b3f75de2145
xinetzone <xinzone@outlook.com>
2023-03-02 16:29:03 +0800

    更新

--- doc/conf.py
+++ doc/conf.py
@@ -173,7 +173,64 @@ html_theme_options = {
     "left_sidebar_end": "sidebar-ethical-ads.html",
     "page_sidebar_items": ["page-toc.html", "edit-this-page.html"],
     # "page_sidebar_items": [], # 删除右侧边栏
-    "footer_items": ["copyright", "sphinx-version", "last-updated", ],
+    "footer_start": ["copyright", "sphinx-version"],
+    "footer_end": ["last-updated", ],
+    # 图标可以参考 https://fontawesome.com/icons
+    "icon_links": [
+        # {
+        #     "name": "GitHub",
+        #     "url": "https://github.com/xinetzone/tvm-book",
+        #     "icon": "fa-brands fa-square-github",
+        #     "type": "fontawesome",
+        # },
+        {
+            "name": "知乎",
+            "url": "https://www.zhihu.com/people/xinetzone",
+            "icon": "fa-brands fa-zhihu",
+            "type": "fontawesome",
+        },
+        {
+            "name": "简书",
+            "url": "https://www.jianshu.com/u/4302480a3e8e",
+            "icon": "fa-solid fa-book",
+            "type": "fontawesome",
+        },
+        {
+            "name": "B站",
+            "url": "https://space.bilibili.com/252192181",
+            "icon": "fa-brands fa-bilibili",
+            "type": "fontawesome",
+        },
+        {
+            "name": "博客园",
+            "url": "https://www.cnblogs.com/q735613050/",
+            "icon": "https://xinetzone.github.io/xinetzone/media/xinetzone.jpg",
+            "type": "url",
+        },
+        {
+            "name": "领英",
+            "url": "https://www.linkedin.com/in/xinet",
+            "icon": "fa-brands fa-linkedin",
+            "type": "fontawesome",
+        },
+        # {
+        #     "name": "GitLab",
+        #     "url": "https://gitlab.com/<your-org>/<your-repo>",
+        #     "icon": "fa-brands fa-square-gitlab",
+        #     "type": "fontawesome",
+        # },
+        # {
+        #     "name": "Twitter",
+        #     "url": "https://twitter.com/<your-handle>",
+        #     "icon": "fa-brands fa-square-twitter",
+        #     # The default for `type` is `fontawesome` so it is not actually required in any of the above examples as it is shown here
+        # },
+        # {
+        #     "name": "Mastodon",
+        #     "url": "https://<your-host>@<your-handle>",
+        #     "icon": "fa-brands fa-mastodon",
+        # },
+    ],
     # "use_download_button": True,
     # "toc_title": "导航",
     # "single_page": True,
